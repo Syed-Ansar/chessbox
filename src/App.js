f8ff964e2622a0a087500902af7db533176c33cf
syedansar <syedansar6666@gmail.com>
2022-07-14 17:27:14 +0530

    Chessbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState(0);
+  const [chess, setChess] = useState([]);
+
+  const Chessboard = () => {
+    let arr = [];
+    for (let i=0;i<input;i++){
+      let temp = [];
+      for (let j=0;j<input;j++){
+        if ((i+j)%2)
+          temp.push(<div className='black'></div>);
+        else  
+          temp.push(<div className = 'white'></div>)
+      }
+      arr.push(temp);
+    }
+    setChess(arr);
+  }
+
+  useEffect(()=>{
+    Chessboard();
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='chess'>
+      
+      <div>
+        <h2> 
+          ChessBoard
+        </h2>
+        <input type="number" placeholder='Enter the Dimension' onChange={(e)=>setInput(e.target.value)} />
+      </div>
+
+      <section className='chessBox' style={{width:100*input}}>
+        {chess}
+      </section>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
